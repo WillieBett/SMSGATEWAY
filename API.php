5ae2989d0816b8efebe8f61286f465339e30a03a
Wilfred Bett <kimutaibett30@gmail.com>
2024-03-11 08:25:42 +0300

    Update API.php

--- API.php
+++ API.php
@@ -9,9 +9,9 @@
 		CURLOPT_TIMEOUT => 30,
 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 		CURLOPT_CUSTOMREQUEST => "POST",
-		CURLOPT_POSTFIELDS => "userid=BROOKHILLSCHOOL&password=Qz5T3nkS&&sendMethod=quick&mobile=25492867363&msg=Test+From+Developer+Team&senderid=BROOKHILL&msgType=text&duplicatecheck=true&output=json",
+		CURLOPT_POSTFIELDS => "userid= userid&password=password&&sendMethod=quick&mobile=mobile&msg=Test+From+Developer+Team&senderid=senderid&msgType=text&duplicatecheck=true&output=json",
 		CURLOPT_HTTPHEADER => array(
-			"apikey:6ee11d9df2cd22ac1e56b098644649a84e4bb250",
+			"apikey: yourapikey",
 			"cache-control: no-cache",
 			"content-type: application/x-www-form-urlencoded"
 		),
